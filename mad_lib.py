6e69b4530e6f404c67c55803f14d7557e02fa323
katerdogg <katiemariedover@gmail.com>
2016-01-07 14:28:47 -0800

    First Doctor Who Mad Lib game made in class

---
+++ mad_lib.py
@@ -0,0 +1,36 @@
+#Make a mad lib that promps user for 5 words/phrases 
+#and then adds those to a silly story.
+
+#madlib = "Start story here " 
+
+#plural_noun = input("Please give a plural noun: ")
+
+#madlib = madlib + plural_noun + "continue story here... "
+
+#-----------------
+
+madlib = "Rose turned around from her place inside the "
+
+adjective = input("Please give an adjective: ")
+
+madlib = madlib + adjective + " TARDIS. She could hear The Doctor screaming in "
+
+adjective = input("Please give another adjective: ")
+
+madlib = madlib + adjective + " jealousy and the "
+
+adjective = input("Please give one more adjective: ")
+
+madlib = madlib + adjective + " laughter that only The Master could accomplish. Rose ran outside where she "
+
+past_verb = input("Please give a past tense verb: ")
+
+madlib = madlib + past_verb + " The Doctor and The Master in "
+
+adjective = input("Please give an adjective: ")
+
+madlib = madlib + adjective + " combat over a sonic screwdriver."
+
+
+print (madlib)
+
