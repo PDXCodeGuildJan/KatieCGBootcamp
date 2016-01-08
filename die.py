ba33815087c65a98070da7017327698975e7ae89
katerdogg <katiemariedover@gmail.com>
2016-01-07 16:19:19 -0800

    random die program with user input for number of sides and dice rolled

--- die.py
+++ die.py
@@ -1,16 +1,26 @@
-# Create a die function that returns a random number as if the user rolled a die
-
 from random import randint
 
-# def die():
-# 	roll = randint (1, 6)
-# 	print (roll)
+def die():
+	roll = randint (1, 6)
+	print (roll)
 
-# die()
+
 
 def custom_die(low, high):
 	roll = randint(low, high)
 	print (roll)
 
 
-custom_die(90, 800)
+
+def main():
+	# ask the user how many dice to roll
+	total_rolls = input("How many dice rolls would you like to roll? ")
+	total_rolls = int(total_rolls)
+	#find out how big the die is
+	max_num = int(input("How many sides on the dice?"))
+	# roll that many dice
+	for amount in range (0, total_rolls):
+		custom_die(1, max_num)
+
+main()
+
