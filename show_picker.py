ee7a38537c9077ed520720569a6a81d7a9393a2a
katerdogg <katiemariedover@gmail.com>
2016-01-08 16:40:07 -0800

    A clean slate for the Tardis Maze Game. Mostly notes.

---
+++ show_picker.py
@@ -0,0 +1,10 @@
+from random import randint
+
+
+#what should you watch?
+def random_show(Sherlock, Supernatural, Detectorists):
+	show = randint("Sherlock", "Supernatural", "Detectorists")
+	print (show)
+
+random_show ()
+
---
+++ tardis_maze_game_new.py
@@ -0,0 +1,21 @@
+def main():
+	print("""You're walking down a busy street when a bright blue old fasioned Police Box catches your eye. 
+	After a brief deliberation, you decide to open its doors and walk inside. You are greeted by a voice on what 
+	you can only guess is some sort of intercom- but that isn't what surprises you. The inside is massive- much
+	larger than what the outside suggested. In fact, it looked to you like the Police Box could go on and on forever;
+	with rooms upon rooms upon rooms that you just wanted to explore. Enter 'quit' to exit the program. """)
+
+	#entry = ""
+
+	#if entry == "quit":
+		#quit()
+
+	console_room()
+
+def console_room():
+
+	print("""The voice spoke. 
+	Hello and welcome to the Tardis! That's Time and Relative Dimension in Space, to you! 
+	The Doctor is out but I will be your virtual host. You have the freedom
+	to go wherever you'd like. There are many rooms to explore. Choose a direction to go and let's get started!
+	You have the ability to go left, forward, or right.""")
