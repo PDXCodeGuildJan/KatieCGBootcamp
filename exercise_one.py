f0fe8b3cb5fba2aae1d435f13024313af2bcde34
katerdogg <katiemariedover@gmail.com>
2016-01-11 14:43:11 -0800

    A program to find the middle three letters in any string

---
+++ exercise_one.py
@@ -0,0 +1,20 @@
+def practice_work():
+	string = "Dumbledore!"
+
+	middle_three_characters(string)
+
+def mid_three_characters(string):
+	mid=(len(string)-3)//2
+	print(string [mid:mid+3])
+
+
+practice_work()
+
+
+
+# 	the_string = len(string_practice)
+# 	print(the_string)
+
+# middle_characters(the_string)
+
+#length divided by two, plus one on either side 
