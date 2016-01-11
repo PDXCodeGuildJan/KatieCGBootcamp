2fd87eb494639b417c41742548a4b650392d907c
katerdogg <katiemariedover@gmail.com>
2016-01-11 15:35:59 -0800

    Finds the middle of a string, prints out each element

--- exercise_one.py
+++ exercise_one.py
@@ -1,12 +1,23 @@
 def practice_work():
+	#can be changed to any string
 	string = "Dumbledore!"
-
+	#calls the mid3 function below, carries string 
 	middle_three_characters(string)
 
-def mid_three_characters(string):
+def middle_three_characters(string):
+	#finds the middle of the string
 	mid=(len(string)-3)//2
+	#finds the two letters surrounding it
 	print(string [mid:mid+3])
 
+	sequence_printer(string)
+
+
+
+def sequence_printer(string):
+	print(string)
+	for x in string:
+		print(x)
 
 practice_work()
 
