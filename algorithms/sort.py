e34e87f9dad1d236f426d080ec871a8813b379a3
katerdogg <katiemariedover@gmail.com>
2016-01-13 16:47:06 -0800

    algorithm folder for all algorithm exercises

---
+++ algorithms/sort.py
@@ -0,0 +1,90 @@
+#Make a function that sorts numbers from lowest to highest
+#then return the list of numbers in numerical order
+
+
+#Define selection_sort with the intention of creating the_list
+def selection_sort(the_list):
+	
+	#the loop will go through each unsorted number in the list looking for the smallest number
+	for lowest_number in the_list:
+		"""go through the_list to find lowest_number"""
+
+		while """the number we're checking""" is > """than the lowest number in in sorted_list but""" < """ and all the numbers in unsorted_list""":
+			"""put that number in the next available spot of the sorted_list """
+			
+
+
+
+
+	
+
+
+
+	#the loop will go through each unsorted number in the list looking for the smallest number
+	
+	#If an unsorted number in the list is smaller than the smallest unsorted number found thus far,
+
+
+	
+
+
+	#the loop will track the new smallest number
+	#Once the list has been processed and the numbers have all checked,
+	#the smallest number will swap to the first spot of the unsorted list
+
+	#Update the start of the unsorted list to be the next spot
+
+	#the loop will repeat this process until the unsorted list is empty.
+
+	
+	#return new sorted list from smallest to largest
+	############
+	#return the_list
+	############
+
+
+
+#sorted = selection_sort([1, 5, 10, 2, 7])
+#print(sorted)
+
+
+	###print(the_list)
+selection_sort([1, 5, 10, 2, 7])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""	unsorted_list = len(the_list)
+	sorted_list = len(the_list+1)"""
+
+
+
+"""while the_list is > 0 :
+
+			#sorted_list = (len(unsorted_list +1"""
+
+
+
+
+"""
+		for lowest_number in the_list:
+			lowest_number = unsorted_list"""
+
+
+
+"""unsorted_list = len
+		sorted_list = len(the_list-1)"""
+
+
+#unsorted_start = 0
+		#while unsorted_start <0:
