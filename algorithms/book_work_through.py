990f0cd45baf10703dfef966958bbfc73ebe3acd
katerdogg <katiemariedover@gmail.com>
2016-01-18 22:42:36 -0800

    exercises provided from Python book that I have worked through

---
+++ algorithms/book_work_through.py
@@ -0,0 +1,6 @@
+def temp_converter():
+	celsius = eval(input("What is the Celsius temperature? "))
+	fahrenheit = 9/5 * celsius + 32
+	print("The temperature is", fahrenheit, "degrees fahrenheit. :)")
+
+temp_converter()
