e8d5d5b971fed0a3d5195692a3c129b3077312a3
katerdogg <katiemariedover@gmail.com>
2016-02-05 15:55:30 -0800

    modified hero module

--- monsters/hero.py
+++ monsters/hero.py
@@ -8,14 +8,16 @@ class Hero(Creature):
 		super(Hero, self).__init__()
 		self.level = 1
 
-
+# Creates MonsterHero class that inherits properties from Monster and Hero classes
 class MonsterHero(Monster, Hero):
 	"""The class MonsterHero inherits both Monster and Hero
 		classes. Any creature created with the MonsterHero class 
 		will now have attributes from both Monster and Hero classes."""
 
 	def __init__(self):
+		# Takes this instance(self) of Monster and adds all init properties from Monster class
 		Monster.__init__(self)
+		# Takes this instance(self) of Monster and adds all init properties from Monster class
 		Hero.__init__(self)
 		#MonsterHero to have a second weapon
 		self.second_weapon = "second weapon!"
