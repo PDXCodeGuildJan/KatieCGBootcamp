85e95f0f1d465da35812d25afe93fced9b4f0400
katerdogg <katiemariedover@gmail.com>
2016-02-05 12:39:52 -0800

    hero module with hero and monster hero classes

--- monsters/hero.py
+++ monsters/hero.py
@@ -10,10 +10,13 @@ class Hero(Creature):
 
 
 class MonsterHero(Monster, Hero):
+	"""The class MonsterHero inherits both Monster and Hero
+		classes. Any creature created with the MonsterHero class 
+		will now have attributes from both Monster and Hero classes."""
 
 	def __init__(self):
 		Monster.__init__(self)
 		Hero.__init__(self)
+		#MonsterHero to have a second weapon
 		self.second_weapon = "second weapon!"
 
-	#Monster Hero to have a second weapon
