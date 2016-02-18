fe35c04e8b260987ca33e92a5025a48caed59845
katerdogg <katiemariedover@gmail.com>
2016-02-18 10:36:33 -0800

    Linked List Exercise

--- linked_list.py
+++ linked_list.py
@@ -6,6 +6,27 @@ C. Removes nodes from anywhere inside a Linked List """
 
 __author__ = "Katie Dover"
 
+def main():
+   link = LinkedList()
+   link.add(4)
+   link.add(2)
+   link.add(86)
+   link.add(4)
+   link.add(7)
+   print("Current list:", link)
+   link.remove(4)
+   print("List after first 4 is removed:", link)
+   link.remove(4)
+   print("List after second 4 is removed:", link)
+   print("Success! Attempt to remove a third 4 yielded no crashes!")
+   link.remove(4)
+   
+   # Output of above code:
+   # -------------------------
+   # Current list: 4 2 86 4 7 
+   # List after first 4 is removed: 2 86 4 7 
+   # List after second 4 is removed: 2 86 7 
+   # Attempt to remove a third 4 yielded no crashes
 
 class Node:
 
@@ -16,10 +37,11 @@ class Node:
 	def __str__(self):
 		return str(self.value)
 
-class Linked_List:
+class LinkedList:
 
 	def __init__(self):
 		self.head = None
+
 
 	def search(self, value):
 		""" Search a linked list by value."""
@@ -30,16 +52,60 @@ class Linked_List:
 		# Create node to be added to end of Linked List
 		new_node = Node(value)
 		
+		# If the there is no head (or no Linked list)
 		if self.head == None:
+			# Create head
 			self.head = new_node 
-		# Find end of linked list
+		# If there is an existing head in the LL:
 		else:
+			# the current node is now the head
 			current_node = self.head
+			# While the next node in current node does not = None:
 			while current_node.next_node != None:
 				current_node = current_node.next_node
 
 			# Append it to the end of existing Linked List
 			current_node.next_node = new_node
+
+	
+	def remove(self, value):
+		""" Remove a node from anywhere inisde the Linked List
+			and re-link new list. """
+
+		# Take already existing node(s) created by add() and remove them from the Linked List
+		# Create current node, previous node, and the one you're searching
+		curr_node = self.head
+		prev_node = None
+		node_search = False
+		#Loop (or search) through the linked list to find the node to remove
+		while curr_node and node_search is False:
+			# If the node we're searching for is in the list
+			# node_search is now True
+
+		# If the curr_node is None:
+			# The value is not in the list
+
+		### End of exercise thoughts### 
+
+		# Like I explained in the code, I needed to loop through the linked list to locate
+		# the node to remove, assign the previous node to the node located directly after
+		# The node to remove, and remove the node. 
+		# In the while loop, I needed to loop through the LL to find the node to search for 
+		# 
+
+		###  ###
+
+
+		# Logic: 
+		# Because nodes always need something to point to...
+		# To remove b:
+		# First get A point to what B is pointing to- C.
+
+		# A's pointer will now point to C(no longer B), B is still pointing to C
+
+		# Once A is pointing to C
+
+		# We can now remove B! A is still A, C is now B, D is now C, and so on and so forth. 
 
 	def __str__(self):	
 		temp_list = []
@@ -53,20 +119,12 @@ class Linked_List:
 		return str(temp_list)
 		
 
-	def remove(self, value):
-		""" Remove a node from anywhere inisde the Linked List
-			and re-link new list. """
-		pass
+		
 
 
 
-my_list = Linked_List()
-
-my_list.add("Item 1")
-my_list.add("Item 2")
-my_list.add("Item 3")
-my_list.add("Item 4")
-print(my_list)
+if __name__ == '__main__':
+	main()
 
 
 
