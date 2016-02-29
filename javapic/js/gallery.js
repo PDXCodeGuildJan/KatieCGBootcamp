4713161cf5d4097178c322633b59cc900df9a8d2
katerdogg <katiemariedover@gmail.com>
2016-02-29 11:22:23 -0800

    work on javapic

--- javapic/js/gallery.js
+++ javapic/js/gallery.js
@@ -21,16 +21,14 @@
  /* ----------------------------------------- */
 
 
-function addName () {
-	var userName = document.getElementsByClassName("tagline");
+// function addName () {
+// 	var userName = document.getElementsByClassName("tagline");
 
-	var newTagline = userName + "your FACE";
+// 	var newTagline = userName + "your FACE";
 
-	console.log("Your FACE");
+// 	console.log("Your FACE");
 
-
-
-}
+// }
 
 
 
@@ -48,7 +46,6 @@ function galleryDisplay() {
 
 			imageGallery = "<li><img src='images/pdxcg_0" + imageIndex + ".jpg' /></li>"
 		} else if (imageIndex === 42) {
-			// imageIndex ++
 			imageGallery = ""
 		
 		} else {
@@ -66,6 +63,37 @@ function galleryDisplay() {
 	imageList += "</ul>"
 	imageGrid.innerHTML = imageList
 
+
+};
+
+document.getElementById("gallery").onclick 	= imageEnlarge;
+// document.getElementsByClassName("display_img").addEventListener("click", imageEnlarge);
+// document.getElementsByClassName("display_none").addEventListener("click", imageRevert);
+
+function imageEnlarge() {
+	console.log("The imageEnlarge is now reporting to the console for duty, ma'am!");
+	// get imageEnlarge to display the class "display_img" when a photo is clicked
+	// something like (but not syntactically) onlick class = "display_img"?
+
+	// find the div and target the class
+	enlargePhoto = document.querySelectorAll("main > div > .display_img")
+	console.log("enlargePhoto??: ", enlargePhoto)
+
+
+	// var imageEnlarge = document.getElementsByClassName("display_none").style.display = "inline-block";
+
+	// 	imageEnlarge.onclick=SelectImage();
+
+
+
+
+};
+
+function imageRevert() {
+	console.log("The imageRevert is now reporting to the console for duty, your highness!")
+
+	// then have imageEnlarge revert back to "display_none" when the surrounding area of the lightbox is clicked
+	// something like (but not syntactically) onlick class = "display_none"?
 
 };
 
