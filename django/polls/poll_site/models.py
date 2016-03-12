d03f97acb873ef3d79e9628d8c5ed4ae52b7a502
katerdogg <katiemariedover@gmail.com>
2016-03-11 18:47:56 -0800

    poll site work with models

--- django/polls/poll_site/models.py
+++ django/polls/poll_site/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+
+class Question(models.Model):
+	"""Defines the Question class and model."""
+	# Establish global variables for the class Quesetion
+	
+	# Define the attributes of the Question Model
+	# 	that are stored in the database
+
+	# Create a text field with a limit of 200 characters
+	question_text = models.CharField(max_length=200)
+	# Create a time stamp labeled "Date Published"
+	pub_date = models.DateTimeField("Date Published")
+
+	def __str__(self):
+		return self.question_text
+
+
+class Choice(models.Model):
+	"""Defines the Choice class and Model."""
+	# Create a text field for each choice to go with the question
+	choice_text = models.CharField(max_length=150)
+	# Create a basic incrementer to count each vote for each choice
+	votes = models.PositiveSmallIntegerField(default=0)
+	# Relationship between choice and question
+	question_id = models.ForeignKey(Question)
+
+	def __str__(self):
+		return self.choice_text
+
--- django/polls/poll_site/urls.py
+++ django/polls/poll_site/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from .views import *
 
 urlpatterns = [
-	url(r'^corgis$', corgi_page, name="corgis"),
+	url(r'corgis$', corgi_page, name="corgis"),
+	url(r'question(?P<question_id>[0-9]+)', question_details, name="question_details"),
 ]
