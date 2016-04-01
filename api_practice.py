1ae0862ac6513389e87f0cf81ae1a935e9864676
katerdogg <katiemariedover@gmail.com>
2016-04-01 14:44:08 -0700

    add a new line of data to print out for each recipe matching the user search. comments added to each chunk of code to better explain what is going on.

--- api_practice.py
+++ api_practice.py
@@ -1,3 +1,7 @@
+"""
+API practice and tests for Cupboard capstone site
+"""
+
 import requests
 import json
 from yummly import Client
@@ -17,8 +21,15 @@ from yummly import Client
 TIMEOUT = 5.0
 RETRIES = 0
 
+# Access the api using the client api id & key
 client = Client(api_id="243f40b9", api_key="0b99d35c38f2043e163a04a97e9c5476", timeout=TIMEOUT, retries=RETRIES)
-	
+
+# Dict of all search parameters. This will be implemented into Cupboard with replacable data. 
+	# Examples:
+	# If a user would like to use pork chops, lemon, and pepper an their dish, it will be included in the "q" param
+	# If a user would like to exclude any ingredients, "excludedIngredient[]" will include those 
+		# specifications in an array.
+	# flavor.meaty.min/max: The allowed savoriness of the dish they're searching for.
 params = {
     'q': 'pork chops',
     'start': 0,
@@ -36,10 +47,14 @@ params = {
     'nutrition.FAT.max': 15
 }
 
+# The var results holds each parameter the user specifies 
 results = client.search(**params)
+
+# Formats the raw json data of all matching recipes
 print(json.dumps(results.matches, indent=4, sort_keys=True))
 # print(results.matches)
 
+# prints the total matches, the recipe id, ingredients, etc. by grabbing the data
 print('Total Matches:', results.totalMatchCount)
 for match in results.matches:
     print('Recipe ID:', match.id)
@@ -50,5 +65,6 @@ for match in results.matches:
     print('Recipe:', match.recipeName)
     print('Rating:', match.rating)
     print('Total Time (mins):', match.totalTimeInSeconds / 60.0)
+    print('Source:', match.sourceDisplayName)
     print('----------------------------------------------------')
 
