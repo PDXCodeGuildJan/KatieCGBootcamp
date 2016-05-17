e57a2ec483fe284a50b8c8825939794fba3d1e06
katerdogg <katiemariedover@gmail.com>
2016-05-16 19:42:32 -0700

    hackerrank practice swap case

---
+++ HackerRank/hr_day_1.py
@@ -0,0 +1,10 @@
+# sWAP cASE
+
+in_one = input("Type a phrase to be replaced: ")
+
+print(("This is your original phrase: ") + in_one)
+new_phrase = "This is your phrase swapped: " + in_one.swapcase()
+
+print(new_phrase)
+
+# 
