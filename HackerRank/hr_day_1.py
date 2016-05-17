ab0dfeff3b530673d6c70a669a72937f07dc89ab
katerdogg <katiemariedover@gmail.com>
2016-05-16 20:16:45 -0700

    beginning of mutations

--- HackerRank/hr_day_1.py
+++ HackerRank/hr_day_1.py
@@ -1,24 +1,32 @@
 # sWAP cASE
 def in_one():
-	in_one = input("Type a phrase to be replaced: ")
+    in_one = input("Type a phrase to be replaced: ")
 
-	print(("This is your original phrase: ") + in_one)
-	new_phrase = "This is your phrase swapped: " + in_one.swapcase()
+    print(("This is your original phrase: ") + in_one)
+    new_phrase = "This is your phrase swapped: " + in_one.swapcase()
 
-	print(new_phrase)
+    print(new_phrase)
+
 
 # String Split and Join
 def string_split():
 
-	orig_string = input("Type a phrase of two or more words: ")
+    orig_string = input("Type a phrase of two or more words: ")
 
-	split_string = orig_string.split(" ")
+    split_string = orig_string.split(" ")
 
-	print(split_string)
+    print(split_string)
 
-	join_string = "-".join(split_string)
+    join_string = "-".join(split_string)
 
-	print(join_string)
+    print(join_string)
 
 
-string_split()
+# Mutations
+def mutations():
+
+    ex_string = "example"
+
+    print(ex_string[5])
+
+mutations()
