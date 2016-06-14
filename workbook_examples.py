887e8bf0fe6031b2dd9f05e9a888f6cffd47b4ca
katerdogg <katiemariedover@gmail.com>
2016-06-14 15:35:33 -0700

    More workbook examples for tutoring

--- workbook_examples.py
+++ workbook_examples.py
@@ -8,3 +8,31 @@ my_list = ["cup", "backpack", "keys", "charger"]
 print(my_list)
 my_list.append("backpack")
 print(my_list)
+
+# remove from a list
+
+my_list.remove("backpack")
+print(my_list)
+
+# Indexing a list
+
+print(my_list[0])
+print(my_list[0:3])
+print(my_list)
+
+# Simple Dictionary Demo
+
+my_dict = {"name": "Katie", "eyes": "Blue"}
+print(my_dict)
+
+# Add to a Dictionary
+
+my_dict["Hair"]  = "blonde"
+print(my_dict)
+
+# Look up a value using a key
+print(my_dict["name"])
+
+del my_dict["name"]
+print(my_dict)
+
